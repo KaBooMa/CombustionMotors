802d789c2f904f81601b7a6af0234a6531bac866
KaBooMa <kaboomaz@gmail.com>
2023-12-30 06:58:05 -0600

    initial calculation adjustments with throttle input

--- Parts/Timing_Sensor.cs
+++ Parts/Timing_Sensor.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using GearLib.Behaviours;
 using GearLib.Behaviours.Fields;
@@ -35,45 +36,42 @@ class Timing_Sensor : Part
 public class TimingSensorBehaviour : BehaviourBase 
 {
     public IntField timing = new IntField("Timing", "Offset degrees from TDC to fire piston", 5, -30, 30);
-    public IntField fire_length = new IntField("Detonation Span", "Degrees span from timing to fire piston", 90, 0, 180);
-    public IntField detonation_power_big = new IntField("Detonation Power", "Amount of power to detonate with", 0, 0, 50000);
-    public FloatField detonation_power_fine = new FloatField("Detonation Power (Fine Tune)", "Small number fine tuning of power", 0, 0, 100);
-    
-    // public BooleanField opposing_force = new BooleanField("Opposing Force", "If enabled, will apply detonation to fixed part as well", false);
+    public IntField piston_offset = new IntField("Piston Offset", "Offsets the TDC for the piston", 0, 0, 360);
+    public FloatField idle_power = new FloatField("Idle Power", "Power to supply during idle", 0, 0, 0.5f);
+    public FloatField max_power = new FloatField("Max Power", "Power to supply at full throttle", 0, 0, 1000);
+    public JoystickField throttle_joystick = new JoystickField("Linear Throttle", "Throttle input");
+    float bore_size = 50f; // mm
+    float stroke_length = 63.1f; // mm
+
+    bool has_fired = false;
 
     void FixedUpdate()
     {
         List<GameObject> attached_pistons = GetLinkedParts("Pistons");
         if (attached_pistons.Count == 0) return;
 
-        float detonation_power = detonation_power_fine + detonation_power_big;
+        float throttle = throttle_joystick.AxisValue;
 
+        int piston_count = 1;
+        float displacement = 0.785f * (float)Math.Pow(bore_size, 2) * stroke_length * piston_count;
+        float calculated_power = displacement / 1000 * idle_power + ((max_power - idle_power) * throttle);
+
+        GameObject piston = attached_pistons[0];
         RotaryBearingAttachment piston_attachment = GetAttachment("CrankSensor").Cast<RotaryBearingAttachment>();
         float crank_angle = piston_attachment.CurrentAngle + piston_attachment.CurrentAngle < 0 ? piston_attachment.CurrentAngle + 360 : piston_attachment.CurrentAngle;
-        
-        float piston_offset = 360 / attached_pistons.Count;
 
-        int current_piston = 0;
-        foreach (GameObject piston in attached_pistons)
+        float start_fire = timing + piston_offset;
+        float stop_fire = (start_fire + 180 - timing) % 360;
+
+        if (!has_fired && crank_angle >= start_fire && crank_angle <= stop_fire)
         {
-            float start_fire = (piston_offset * current_piston) + timing;
-            float stop_fire = start_fire + fire_length;
-
-            if (crank_angle >= start_fire && crank_angle <= stop_fire)
-            {
-                Rigidbody piston_rigid_body = piston.transform.parent.GetComponent<Rigidbody>();
-                piston_rigid_body.AddRelativeForce(-Vector3.up * detonation_power);
-
-                //piston_rigid_body.velocity = -piston_rigid_body.transform.up * Time.deltaTime * detonation_power;
-                
-                // if (opposing_force) 
-                // {
-                //     FixedAttachment fixed_attachment = GetAttachment("Fixed").Cast<FixedAttachment>();
-                //     Rigidbody attached_rigid_body = fixed_attachment.ConnectedPart.parentComposite.GetComponent<Rigidbody>();
-                //     attached_rigid_body.velocity = -attached_rigid_body.transform.forward * Time.deltaTime * detonation_power;
-                // }
-            }
-            current_piston++;
+            Rigidbody piston_rigid_body = piston.transform.parent.GetComponent<Rigidbody>();
+            piston_rigid_body.AddRelativeForce(-Vector3.up * calculated_power);
+            has_fired = true;
+        }
+        else
+        {
+            has_fired = false;
         }
     }
 }
